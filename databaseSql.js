1311cf403bfa1c58ff85407982c7f95eb388e71d
Andrew971 <sordier.andrew@gmail.com>
2018-03-07 23:52:19 -0800

    ok

--- databaseSql.js
+++ databaseSql.js
@@ -1,6 +1,8 @@
 const knex = require('knex')({
   client: 'pg',
-  connection:'postgres://pvdktlsqiflocg:6f70b41436be046b08583f359560648a260ccdfed5aa4896bd153d508f7f38c7@ec2-50-17-206-214.compute-1.amazonaws.com:5432/dd4jl772brs562'
+  connection: {
+    filename: 'postgres://pvdktlsqiflocg:6f70b41436be046b08583f359560648a260ccdfed5aa4896bd153d508f7f38c7@ec2-50-17-206-214.compute-1.amazonaws.com:5432/dd4jl772brs562'
+  }
 //   connection: {
 //     database: 'todoApp',
 //     user:     'andrewsordier',
