05d1abdedfe2f028abadfcc6c7744e72901a4165
Andrew971 <sordier.andrew@gmail.com>
2018-03-08 00:10:21 -0800

    l

--- databaseSql.js
+++ databaseSql.js
@@ -1,6 +1,6 @@
 var environment = process.env.NODE_ENV || 'development';
 var config = require('./knexfile.js')[environment];
-const bookshelf = require('bookshelf')(knex)
+const bookshelf = require('bookshelf')(config)
 
 
 module.exports = bookshelf
