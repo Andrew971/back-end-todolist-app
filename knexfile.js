480fffa5eafcd53c58a1d9927bf774d12fee6a0d
Andrew971 <sordier.andrew@gmail.com>
2018-03-07 23:44:46 -0800

    ok

--- knexfile.js
+++ knexfile.js
@@ -27,7 +27,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection:'postgres://forlkxvljdpimg:3110c26991f3e573d725fafc8f229a52ddcb492e489abba6d15952928e18f2cf@ec2-54-243-239-66.compute-1.amazonaws.com:5432/dd3veias0vr4e2',
+    connection:'postgres://pvdktlsqiflocg:6f70b41436be046b08583f359560648a260ccdfed5aa4896bd153d508f7f38c7@ec2-50-17-206-214.compute-1.amazonaws.com:5432/dd4jl772brs562',
     pool: {
       min: 2,
       max: 10
--- migrations/20180307131741_users.js
+++ migrations/20180307131741_users.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('users', function (table) {
     table.increments('id').primary(); // adds incrementing int for id
-    table.unique('Username').notNullable()
+    table.string('Username').notNullable()
     table.string('Password').notNullable()
 })
 };
