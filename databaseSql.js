f380d33136d94ee205ef4e3d4550796a2bac6ebf
Andrew971 <sordier.andrew@gmail.com>
2018-03-08 00:41:02 -0800

    ok

--- databaseSql.js
+++ databaseSql.js
@@ -1,5 +1,13 @@
-var environment =process.env.NODE_ENV || 'development';
-const knex = require('knex')([environment]);
+
+const knex = require('knex')({
+  client: 'pg',
+  connection:{database:'postgres://pvdktlsqiflocg:6f70b41436be046b08583f359560648a260ccdfed5aa4896bd153d508f7f38c7@ec2-50-17-206-214.compute-1.amazonaws.com:5432/dd4jl772brs562'}
+//   connection: {
+//     database: 'todoApp',
+//     user:     'andrewsordier',
+//     password: 'password'
+// }
+});
 const bookshelf = require('bookshelf')(knex)
 
 
